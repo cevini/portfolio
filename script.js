7e4aa09a0e21b2d635e00f7a5f72193858d9a6be
Vinícius Mangeth <106355859+cevini@users.noreply.github.com>
2023-06-26 21:28:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,32 @@
-//Portfólio (Filter)
-
-let list = document.querySelectorAll('.list');
-let itemBox = document.querySelectorAll('.itemBox');
-
-for(let i = 0; i<list.length; i++){
-  list[i].addEventListener('click', function(){
-    for(let j = 0; j<list.length; j++){
-      list[j].classList.remove('active');
-    }
-    this.classList.add('active');
-
-    let dataFilter = this.getAttribute('data-filter');
-
-    for( let k = 0; k<itemBox.length; k++){
-      itemBox[k].classList.remove('active');
-      itemBox[k].classList.add('hide');
-
-      if(itemBox[k].getAttribute('data-item') == dataFilter ||
-      dataFilter == "tudo"){
-        itemBox[k].classList.remove('hide');
-        itemBox[k].classList.add('active')
-      }
-    }
-  })
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: smooth });
 }
 
-//Contato (Sistema de envio de email e validação)
+//Slide Tag (Posts / Portfólio)
 
-function validate() {
-  let name = document.querySelector(".name")
-  let email = document.querySelector(".email")
-  let msg = document.querySelector(".message")
-  let sendBtn = document.querySelector(".send-btn")
+const content = document.querySelector(".content");
+const postsBtn = document.querySelector(".posts-btn");
+const portfolioBtn = document.querySelector(".portfolio-btn");
 
-  sendBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(name.value == "" || email.value == "" || msg.value == ""){
-      emptyerror();
-    } else{
-      sendmail (name.value, email.value, msg.value);
-      success();
-    }
-  })
+function moveToRight() {
+  content.style.transform = "translateX(-100vw)";
+  portfolioBtn.style.background = "#b5b5b5";
+  postsBtn.style.background = "#dfdfdf";
 }
-validate();
-
-function sendmail(name,email,msg){
-  emailjs.send("service_h0ob2bm","template_gn4bv8f",{
-    from_name: email,
-    to_name: name,
-    message: msg,
-  });
+function moveToLeft() {
+  content.style.transform = "translateX(0vw)";
+  postsBtn.style.background = "#b5b5b5";
+  portfolioBtn.style.background = "#dfdfdf";
 }
 
-function emptyerror(){
-  swal({
-    title: "Deu erro, hein?",
-    text: "Preencha todos os campos.",
-    icon: "error",
-  })
+//Img Social
+
+const socialBtn1 = document.querySelector(".btn-social1");
+const socialBtn2 = document.querySelector(".btn-social2");
+const socialBtn3 = document.querySelector(".btn-social3");
+
+function showSocialBtn() {
+  socialBtn1.style.transform = "translateY(-8rem)";
+  socialBtn2.style.transform = "translateY(-7rem) translateX(-4rem)";
+  socialBtn3.style.transform = "translateY(-7rem) translateX(4rem)";
 }
-function success(){
-  swal({
-    title: "Foi!",
-    text: "Em breve darei retorno :)",
-    icon: "success",
-  })
-}
-
-//Efeito scroll para cada item da Barra de navegação
-
-const menuItems = document.querySelectorAll('.menu li');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', scrollToIdOnClick);
-})
-
-function scrollToIdOnClick(event) {
-  event.preventDefault();
-  const to = getScrollTopByHref(event.target);
-  scrollToPosition(to);
-}
-
-function scrollToPosition(to){
-  // window.scroll({
-  //   top: to,
-  //   behavior: "smooth",
-  // });
-  smoothScrollTo(0, to);
-}
-
-
-function getScrollTopByHref(element){
-  const id = element.getAttribute('href');
-  return document.querySelector(id).offsetTop;
-}
-
-        /**
-     * Smooth scroll animation
-     * @param {int} endX: destination x coordinate
-     * @param {int} endY: destination y coordinate
-     * @param {int} duration: animation duration in ms
-     */
-    function smoothScrollTo(endX, endY, duration) {
-      const startX = window.scrollX || window.pageXOffset;
-      const startY = window.scrollY || window.pageYOffset;
-      const distanceX = endX - startX;
-      const distanceY = endY - startY;
-      const startTime = new Date().getTime();
-
-      duration = typeof duration !== 'undefined' ? duration : 600;
-
-      // Easing function
-      const easeInOutQuart = (time, from, distance, duration) => {
-        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-      };
-
-      const timer = setInterval(() => {
-        const time = new Date().getTime() - startTime;
-        const newX = easeInOutQuart(time, startX, distanceX, duration);
-        const newY = easeInOutQuart(time, startY, distanceY, duration);
-        if (time >= duration) {
-          clearInterval(timer);
-        }
-        window.scroll(newX, newY);
-      }, 1000 / 60); // 60 fps
-    };
